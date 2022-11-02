cf5f64e3b29bc835a318ed628ef664e70b80c675
criley6nd <criley6@nd.edu>
2022-11-01 22:28:27 -0400

    change cors and logs

--- get-orders/__init__.py
+++ get-orders/__init__.py
@@ -15,7 +15,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     # Connect to MySQL
     try:
-        logging.info(req.get_json())
+        logging.info(req.get_json()['sql'])
     except:
         logging.info("No json data")
     cnx = mysql.connector.connect(**config  
